a218a1250a6dbd4d98c0a78d5571ce9424aeb92e
Raamprasaadh <ramprasadhsurya@gmail.com>
2021-01-18 17:04:06 +0530

    adds functionality of add and view password

--- client/src/components/addPasswordComponent.js
+++ client/src/components/addPasswordComponent.js
@@ -1,12 +1,129 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class addPasswordComponent extends Component {
+    constructor(props)
+        {
+            super(props);
+
+            this.state={
+             name:"",
+             url:"",
+             userName:"",
+             pwd:"",
+             type:"",
+             types:[]
+            }
+            this.onChangeName = this.onChangeName.bind(this);
+            this.onChangeUrl = this.onChangeUrl.bind(this);
+            this.onChangeuserName = this.onChangeuserName.bind(this);
+            this.onChangePwd = this.onChangePwd.bind(this);
+            this.onChangeType = this.onChangeType.bind(this);
+            this.onSubmit = this.onSubmit.bind(this);
+}
+
+onChangeName(e){
+this.setState({
+    name:e.target.value
+});
+}
+onChangePwd(e){
+this.setState({pwd:e.target.value});
+}
+onChangeType(e){
+this.setState({type:e.target.value});
+}
+onChangeUrl(e){
+this.setState({url:e.target.value});
+}
+onChangeuserName(e){
+this.setState({userName:e.target.value});
+}
+onSubmit(e){
+e.preventDefault();
+    const password = {
+    name: this.state.name,
+    url: this.state.url,
+    userName:this.state.userName,
+    pwd:this.state.pwd,
+    type:this.state.type
+}
+console.log("password");
+console.log(password);
+axios.post('http://localhost:3001/password/add',password)
+.then(res=>console.log(res.data))
+.catch(err=>console.error(err));
+}
+
+componentDidMount(){
+            axios.get('http://localhost:3001/type')
+            .then(res=>{
+                if(res.data.length >0)
+                {
+                this.setState({
+                    types: res.data.map(type=>type.name)
+                });
+            }
+            })
+}
+
     render() {
-        return (
+               return (
             <div>
                 <div className="pageTitle">
                     Add Password page
                 </div>
+                <form onSubmit={this.onSubmit}>
+                <div className="formGroup">
+                    <div className="formLabel">
+                        <label>Name :</label>
+                    </div>
+                    <div className="formInput">
+                        <input required type="text" onChange = {this.onChangeName}placeholder="eg. office mail" />
+                    </div>
+                    </div>
+                    <div className="formGroup">
+                    <div className="formLabel">
+                        <label>Url :</label>
+                    </div>
+                    <div className="formInput">
+                        <input required type="text" onChange = {this.onChangeUrl} placeholder="eg. www.gmail.com" />
+                    </div>
+                   </div>
+                   <div className="formGroup">
+                    <div className="formLabel">
+                        <label>Username :</label>
+                    </div>
+                    <div className="formInput">
+                        <input required type="text" onChange={this.onChangeuserName} placeholder="eg. example@gmail.com" />
+                    </div>
+                </div>
+                   <div className="formGroup">
+                    <div className="formLabel">
+                        <label>Password :</label>
+                    </div>
+                    <div className="formInput">
+                        <input required type="password" onChange={this.onChangePwd}  />
+                    </div>
+                </div>
+                <div className="formGroup">
+                    <div className="formLabel">
+                        <label>Type :</label>
+                    </div>
+                    <div className="formInput">
+                        <select required onChange={this.onChangeType} >
+                            <option>select</option>
+                            {
+                                this.state.types.map(type=>{
+                                    return <option key={type}
+                                    value={type}>{type}</option>
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+                <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
--- client/src/components/viewPasswordComponent.js
+++ client/src/components/viewPasswordComponent.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class viewPasswordComponent extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            passwords:[]
+        }
+    }
+    componentDidMount(){
+        axios.get("http://localhost:3001/password")
+        .then(res=>this.setState({passwords:res.data}));
+    }
     render() {
         return (
             <div>
                 <div className="pageTitle">
                     View Password page
+                </div>
+                <div className="displayPasswords">
+                    <table>
+                        <thead>
+                            <td>Name</td>
+                            <td>Url</td>
+                            <td>User Name</td>
+                            <td>Password</td>
+                            <td>Type</td>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.passwords.map(password=>{
+                                    return <tr>
+                                        <td>{password.name}</td>
+                                        <td>{password.url}</td>
+                                        <td>{password.userName}</td>
+                                        <td>{password.password}</td>
+                                        <td>{password.type}</td>
+                                    </tr>
+                                })
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
